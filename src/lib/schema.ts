43e96b6e9b44dd2ec8d2198341d33e7e0172c6c4
CrustyBanana <dodoankhac@gmail.com>
2025-07-10 13:05:18 +0700

    feat: update db schemas

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -3,9 +3,32 @@ import { z } from 'zod';
 
 export const UserSchema = z.object({
     _id: z.instanceof(ObjectId).optional(),
-    name: z.string().min(1, 'Full name is required'),
+    firstName: z.string().min(1, 'First name is required'),
+    lastName: z.string().min(1, 'Last name is required'),
+    role: z.enum(['admin', 'user']).default('user'),
     email: z.string().email('Invalid email address'),
     password: z.string().min(6, 'Password must be at least 6 characters'),
 });
 
 export type User = z.infer<typeof UserSchema>;
+
+const AllergySchema = z.object({
+    _id: z.instanceof(ObjectId).optional(),
+    name: z.string(),
+    symptoms: z.array(z.string()),
+    treatment: z.string(),
+    firstAid: z.string(),
+    allergens: z.array(z.string()),
+});
+
+export type Allergy = z.infer<typeof AllergySchema>;
+
+export const PAPSchema = z.object({
+    _id: z.instanceof(ObjectId).optional(),
+    userId: z.string(),
+    gender: z.enum(['male', 'female', 'other']),
+    doB: z.date().optional(),
+    allergies: z.array(AllergySchema),
+});
+
+export type PAP = z.infer<typeof PAPSchema>;
