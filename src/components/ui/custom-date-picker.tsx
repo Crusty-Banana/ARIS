dcc33c2cc8e7f3c6c74e39afa1bab986ec57c77a
BuiVuBaoMinh <buivubaominhlqd@gmail.com>
2025-09-29 14:22:16 +0700

    fix: DatePicker's Popover display error, clean up

--- src/components/ui/custom-date-picker.tsx
+++ src/components/ui/custom-date-picker.tsx
@@ -13,23 +13,6 @@ import { Popover, PopoverContent, PopoverTrigger } from "@/components/ui/popover
 
 // A custom PopoverContent that DOES NOT use a portal.
 // This ensures it renders inside the Dialog's DOM tree.
-const PopoverContentNoPortal = React.forwardRef<
-  React.ComponentRef<typeof PopoverPrimitive.Content>,
-  React.ComponentPropsWithoutRef<typeof PopoverPrimitive.Content>
->(({ className, align = "center", sideOffset = 4, ...props }, ref) => (
-  <PopoverPrimitive.Content
-    ref={ref}
-    align={align}
-    sideOffset={sideOffset}
-    // Copy the styles from shadcn/ui's PopoverContent.
-    className={cn(
-      "z-50 w-72 rounded-md border bg-popover p-4 text-popover-foreground shadow-md outline-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0 data-[state=closed]:zoom-out-95 data-[state=open]:zoom-in-95 data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2",
-      className,
-    )}
-    {...props}
-  />
-))
-PopoverContentNoPortal.displayName = PopoverPrimitive.Content.displayName
 
 interface DatePickerProps {
   value: Date | undefined
