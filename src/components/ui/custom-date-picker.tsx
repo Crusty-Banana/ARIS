50ea04dc5229889ce2aec3286c20ef60e611ea88
BuiVuBaoMinh <buivubaominhlqd@gmail.com>
2025-09-29 17:10:04 +0700

    fix: remove unused import

--- src/components/ui/custom-date-picker.tsx
+++ src/components/ui/custom-date-picker.tsx
@@ -4,7 +4,6 @@ import * as React from "react"
 import { format } from "date-fns"
 import { vi } from "date-fns/locale/vi"
 import { Calendar as CalendarIcon } from "lucide-react"
-import * as PopoverPrimitive from "@radix-ui/react-popover"
 
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
