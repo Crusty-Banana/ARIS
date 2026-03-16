fef3071daec89db3e4598a78567ddadcef8402b3
BuiVuBaoMinh <buivubaominhlqd@gmail.com>
2026-03-16 10:08:25 +0700

    Add envs to test Amplify

--- src/modules/env/server.ts
+++ src/modules/env/server.ts
@@ -1,5 +1,6 @@
 import { z } from "zod";
 import { CLIENT_ENV, ClientEnv } from "./client";
+import { Server } from "lucide-react";
 
 export const ServerEnv = ClientEnv.extend({
   MONGODB_URI: z.string().startsWith("mongodb"),
@@ -22,4 +23,25 @@ export const SERVER_ENV = ServerEnv.parse({
   AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
   AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
   AWS_S3_BUCKET_NAME: process.env.AWS_S3_BUCKET_NAME,
+  AMPTEST_AWS_ACCESS_KEY_ID: process.env.AMPTEST_AWS_ACCESS_KEY_ID,
+  AMPTEST_AWS_SECRET_ACCESS_KEY: process.env.AMPTEST_AWS_SECRET_ACCESS_KEY,
+  AMPTEST_AWS_S3_BUCKET_NAME: process.env.AMPTEST_AWS_S3_BUCKET_NAME,
+  AMPTEST_AWS_REGION: process.env.AMPTEST_AWS_REGION,
+});
+
+export const AmptestServerEnv = ServerEnv.extend({
+  AMPTEST_AWS_ACCESS_KEY_ID: z.string(),
+  AMPTEST_AWS_SECRET_ACCESS_KEY: z.string(),
+  AMPTEST_AWS_S3_BUCKET_NAME: z.string(),
+  AMPTEST_AWS_REGION: z.string(),
+});
+
+export type AmptestServerEnv = z.infer<typeof AmptestServerEnv>;
+
+export const AMPTEST_SERVER_ENV = AmptestServerEnv.parse({
+  ...SERVER_ENV,
+  AMPTEST_AWS_ACCESS_KEY_ID: process.env.AMPTEST_AWS_ACCESS_KEY_ID,
+  AMPTEST_AWS_SECRET_ACCESS_KEY: process.env.AMPTEST_AWS_SECRET_ACCESS_KEY,
+  AMPTEST_AWS_S3_BUCKET_NAME: process.env.AMPTEST_AWS_S3_BUCKET_NAME,
+  AMPTEST_AWS_REGION: process.env.AMPTEST_AWS_REGION,
 });
--- src/modules/s3-storage/index.ts
+++ src/modules/s3-storage/index.ts
@@ -1,12 +1,13 @@
 import { PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
-import { SERVER_ENV } from "@/modules/env/server";
+//import { SERVER_ENV } from "@/modules/env/server"; // True import
+import { AMPTEST_SERVER_ENV } from "@/modules/env/server"; // Amplify Deployment Test
 import { AddFileToS3$Params } from "../commands/AddFileToS3/typing";
 
 export const s3Client = new S3Client({
-  region: SERVER_ENV.AWS_REGION,
+  region: AMPTEST_SERVER_ENV.AMPTEST_AWS_REGION,
   credentials: {
-    accessKeyId: SERVER_ENV.AWS_ACCESS_KEY_ID,
-    secretAccessKey: SERVER_ENV.AWS_SECRET_ACCESS_KEY,
+    accessKeyId: AMPTEST_SERVER_ENV.AMPTEST_AWS_ACCESS_KEY_ID,
+    secretAccessKey: AMPTEST_SERVER_ENV.AMPTEST_AWS_SECRET_ACCESS_KEY,
   },
 });
 
